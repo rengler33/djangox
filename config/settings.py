0a557930ac388029c136e5f4d345ffdd9b0c6291
rob <rob@pop-os.localdomain>
2019-08-27 08:39:21 -0500

    Adjust settings and manage to be aware of apps folder

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ from loguru import logger
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+APPS_DIR = os.path.join(BASE_DIR, "apps")
 
 # Load environment variables
 env = environ.Env(DEBUG=(bool, False), USE_SQLITE=(bool, False))
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     "allauth.account",
     "crispy_forms",
     # Local
-    "users.apps.UsersConfig",
-    "pages.apps.PagesConfig",
+    "apps.users.apps.UsersConfig",
+    "apps.pages.apps.PagesConfig",
 ]
 if DEBUG:
     INSTALLED_APPS.append("debug_toolbar")
@@ -63,7 +63,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": ["apps/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "/apps/static/"
+STATICFILES_DIRS = [os.path.join(APPS_DIR, "static")]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
--- manage.py
+++ manage.py
@@ -12,4 +12,10 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # This allows easy placement of apps within the interior
+    # apps directory.
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, "apps"))
+
     execute_from_command_line(sys.argv)
