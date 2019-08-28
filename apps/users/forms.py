9c5eff95ee4e94ba93364c6ec132664d9fa4888b
rob <rob@pop-os.localdomain>
2019-08-27 20:27:33 -0500

    Adjust user model reference

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from django.contrib.auth import get_user_model
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        model = CustomUser
+        model = get_user_model()
         fields = ("email", "username")
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = ("email", "username")
