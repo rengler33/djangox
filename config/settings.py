9305a1c9ed058e748030c6eafe19b001f7c02557
rob <rob@pop-os.localdomain>
2019-08-27 21:30:18 -0500

    Add production security measures to settings

--- config/settings.py
+++ config/settings.py
@@ -186,3 +186,15 @@ MESSAGE_TAGS = {
     messages.WARNING: "alert-warning",
     messages.ERROR: "alert-danger",
 }
+
+# Security for production
+if ENV_CONTROLLER == "production":
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = "DENY"
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
