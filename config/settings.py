2ea835bf1d63b72c6d0ff2d0bc72a77f5eaed80a
rob <rob@pop-os.localdomain>
2019-08-27 22:19:08 -0500

    Add django-extensions for development

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 if DEBUG:
     INSTALLED_APPS.append("debug_toolbar")
+    INSTALLED_APPS.append("django_extensions")
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
